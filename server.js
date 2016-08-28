37e44a7c79c34a89fe94b11872ffb625c67ac2a2
j4m1e <jamie@thetylers.me.uk>
2016-08-28 19:42:18 +0100

    Add POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet mum for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to market',
-	completed: false
-}, {
-	id: 3,
-	description: 'Take dog for a walk',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
@@ -37,6 +29,7 @@ app.get('/todos/:id', function (req, res) {
 		}
 	});
 
+	// If matchedTodo is true i.e. got something returned
 	if (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
@@ -44,6 +37,18 @@ app.get('/todos/:id', function (req, res) {
 		res.status(404).send();
 	}
 	//res.send('Asking for todo with id of ' + req.params.id);
+});
+
+// POST /todos
+app.post('/todos', function (req, res) {
+	body = req.body;
+
+	// add id field & increment it afterwards (++)
+	body.id = todoNextId++;
+	// push body into array
+	todos.push(body);
+	// console.log('description: ' + body.description);
+	res.json(body);
 });
 
 app.listen(PORT, function() {
