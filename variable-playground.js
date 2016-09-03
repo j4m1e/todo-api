0d6bc5930b677e276f80d90ff0552922cac9c433
Jamie Tyler <jamie@thetylers.me.uk>
2016-09-03 08:09:59 +0100

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,30 @@
+// var person = {
+// 	name: 'Jamie',
+// 	age: 21
+// }
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Jamie',
+// 	// 	age: 44 
+// 	// };
+// 	obj.age = 44;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array example
+var grades = [15, 37];
+
+function addGrade (gradesArr) {
+	gradesArr.push(55);
+	debugger;
+	// gradesArr = [12, 33, 99];
+
+}
+
+addGrade(grades);
+console.log(grades);
+
+
