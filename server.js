748cc97b1c0fdd23965824d6df09b43a68f2e05b
Jamie Tyler <jamie@thetylers.me.uk>
2016-09-14 15:24:31 +0100

    Added DELETE /users/login

--- server.js
+++ server.js
@@ -274,20 +274,39 @@ app.post('/users', function(req, res) {
 // POST /users/login
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	// class method
 
 	db.user.authenticate(body).then(function (user) {
 		// all went well
 		var token = user.generateToken('authentication');
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());
-		} else {
-			res.status(401).send();
-		}
-	}, function () {
+		userInstance = user;
+
+		// store token in database
+		return db.token.create({
+			token: token
+		});
+
+		// if (token) {
+		// 	res.header('Auth', token).json(user.toPublicJSON());
+		// } else {
+		// 	res.status(401).send();
+		// }
+	}).then(function (tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function () {
 		// went bad - dont want to expose why the authentication worked
 		res.status(401).send();
+	});
+});
+
+// DELETE /users/login i.e. logout
+app.delete('/users/login', middleware.requireAuthentication, function (req, res) {
+	req.token.destroy().then(function() {
+		res.status(204).send();
+	}).catch(function() {
+		res.status(500).send();
 	});
 });
 
